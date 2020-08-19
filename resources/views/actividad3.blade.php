f60ad2bbdea2b934cc6b0e1ccb49cc4d00da91cf
Franco2319 <fgt-1995@hotmail.com>
2020-08-19 15:58:48 -0300

    cambios actividades

--- resources/views/actividad3.blade.php
+++ resources/views/actividad3.blade.php
@@ -12,11 +12,10 @@
     <img class="imgact1mobile" src="/images/imgact3mobile.jpg" alt="">
     <img class="imgact1" src="/images/vistaimgact3.jpg" alt="">
 
-    <p class="actvideop">Luego de ver el video, seguí las siguientes ins- <br /> trucciones para aprender más sobre el tema.</p>
       <div class="video-responsive">  
-        <iframe src="https://www.youtube.com/embed/3cvmONlV5WU" frameborder="0"  allowfullscreen></iframe>
-        
+        <iframe src="https://www.youtube.com/embed/3cvmONlV5WU" frameborder="0"  allowfullscreen></iframe>  
       </div> 
+      <p class="actvideop">Luego de ver el video, seguí las siguientes ins- <br /> trucciones para aprender más sobre el tema.</p>
     </div>
   </div>
 
@@ -25,7 +24,7 @@
 
 <div class="solapas">
 
-<ul class="nav nav-tabs nav-justified act1li" id="myTab" role="tablist">
+<ul class="nav nav-tabs act1li" id="myTab" role="tablist">
   <li class="nav-item ">
     <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">PREGUNTAS DE REVISIÓN</a>
   </li>
@@ -277,10 +276,6 @@
 
 <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
   <div class="actividadindividual">
-  <br>
-  <br>
-  <div class="padlet-embed" style="border:1px solid rgba(0,0,0,0.1);border-radius:2px;box-sizing:border-box;overflow:hidden;position:relative;width:100%;background:#F4F4F4"><p style="padding:0;margin:0"><iframe src="https://padlet.com/embed/hs6fmtjmnc31jpql" frameborder="0" allow="camera;microphone;geolocation" style="width:100%;height:608px;display:block;padding:0;margin:0"></iframe></p><div style="padding:8px;text-align:right;margin:0;"><a href="https://padlet.com?ref=embed" style="padding:0;margin:0;border:none;display:block;line-height:1;height:16px" target="_blank"><img src="https://padlet.net/embeds/made_with_padlet.png" width="86" height="16" style="padding:0;margin:0;background:none;border:none;display:inline;box-shadow:none" alt="Hecho con Padlet"></a></div></div>
-
       
       <h2 class="actividadh2">Actividad grupal</h2>
       <h3> <span class="fa-stack circulo1">
@@ -288,17 +283,18 @@
       <strong class="fa-stack-1x">
         1    
       </strong>
-      </span>Divídanse en equipos para trabajar</h3>
-      <ul class="individualul">
-        <li>
-        Deberán buscar en las redes (Facebook, twitter, Instagram, tik tok) 5 comentarios y posteos donde se reflejen elementos de antisemitismo moderno y péguenlos en el Padlet ofrecido. Luego, piensen cómo responderían a cada posteo y agréguenlos en el Padlet. 
-      </li>
-      <br>
-       <li>
-       Además colaboren con cincos reacciones en los posteos de sus compañeros/as, escribiendo respuestas alternativas en el Padlet. 
-       </li>
-      </ul>
+      </span>Busquen en las redes (Facebook, twitter, Instagram, tik tok) 5 comentarios y posteos donde se reflejen elementos de antisemitismo moderno y péguenlos en el Padlet ofrecido. Luego, piensen cómo responderían a cada posteo y agréguenlos en el Padlet.
+      </h3>
+      <h3> <span class="fa-stack circulo1">
+      <span class="fa fa-circle-o fa-stack-2x "></span>
+      <strong class="fa-stack-1x">
+        2    
+      </strong>
+      </span>Además colaboren con cincos reacciones en los posteos de sus compañeros/as, escribiendo respuestas alternativas en el Padlet.
+      </h3>
       <div class="lineadiv2"></div>
+      <div class="padlet-embed" style="border:1px solid rgba(0,0,0,0.1);border-radius:2px;box-sizing:border-box;overflow:hidden;position:relative;width:100%;background:#F4F4F4"><p style="padding:0;margin:0"><iframe src="https://padlet.com/embed/hs6fmtjmnc31jpql" frameborder="0" allow="camera;microphone;geolocation" style="width:100%;height:608px;display:block;padding:0;margin:0"></iframe></p><div style="padding:8px;text-align:right;margin:0;"><a href="https://padlet.com?ref=embed" style="padding:0;margin:0;border:none;display:block;line-height:1;height:16px" target="_blank"><img src="https://padlet.net/embeds/made_with_padlet.png" width="86" height="16" style="padding:0;margin:0;background:none;border:none;display:inline;box-shadow:none" alt="Hecho con Padlet"></a></div></div>
+
  </div>
 </div>
 
--- resources/views/actividad4.blade.php
+++ resources/views/actividad4.blade.php
@@ -12,11 +12,11 @@
     <img class="imgact1mobile" src="/images/imgact4mobile.jpg" alt="">
     <img class="imgact1" src="/images/vistaimgact4.jpg" alt="">
 
-    <p class="actvideop">Luego de ver el video, seguí las siguientes ins- <br /> trucciones para aprender más sobre el tema.</p>
       <div class="video-responsive">  
         <iframe src="https://www.youtube.com/embed/3cvmONlV5WU" frameborder="0"  allowfullscreen></iframe>
         
-      </div> 
+      </div>
+      <p class="actvideop">Luego de ver el video, seguí las siguientes ins- <br /> trucciones para aprender más sobre el tema.</p> 
     </div>
   </div>
 
@@ -25,7 +25,7 @@
 
 <div class="solapas">
 
-<ul class="nav nav-tabs nav-justified act1li" id="myTab" role="tablist">
+<ul class="nav nav-tabs act1li" id="myTab" role="tablist">
   <li class="nav-item ">
     <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">PREGUNTAS DE REVISIÓN</a>
   </li>
@@ -167,7 +167,6 @@
             <span class="checkmark" id="check14" ></span>
         </label>
          <br>
-         <br>
 
          <label class="label-container" id="resp15" >Remarcando las cuestiones positivas presentes en Israel -como el Estado de las Start Ups- y combatir así la ola de información negativa en contra de los judíos.
             <input id="p5r3" type="radio" name="p5" value="1">
@@ -218,27 +217,30 @@
         1    
       </strong>
       </span>Lean el siguiente caso para luego analizar:</h3>
-      <ul class="individualul">
+      <ul style="font-style: italic;" class="individualul">
         <li>En el periódico más importante de tu ciudad, el periodista Fernando P. publicó un artículo exponiendo una teoría que vincula al Estado de Israel con el brote del Ckzrt-20, un virus sumamente contagioso que provocó una pandemia a nivel mundial. Dicha teoría es absolutamente conspirativa ya que no hay ninguna prueba al respecto y se fundamenta por noticias falsas.
            La nota publicada causó gran revuelo en la ciudad y la réplica de la falsa teoría en varios medios de comunicación: en TV, radio y portales de noticias en internet. A raíz de esto, se observó un gran número de acciones y comentarios antisemitas en las redes y se denunciaron casos aislados de judeofobia en la vía pública y medios de transporte. 
       </li>
-      <br>
-       <li>
-        Construyan una propuesta para trabajar desde vuestra institución para responder a dicho acto antisemita experimentado, desde los distintos enfoques. Elaboren una propuesta teniendo en cuenta estrategias múltiples para responder ante diversos públicos (comunicacional, jurídico, educativo, etc.)
-       </li>
       </ul>
+      <h3> <span class="fa-stack circulo1">
+      <span class="fa fa-circle-o fa-stack-2x "></span>
+      <strong class="fa-stack-1x">
+        2
+      </strong>
+      </span>Construyan una propuesta para trabajar desde vuestra institución para responder a dicho acto antisemita experimentado, desde los distintos enfoques. Elaboren una propuesta teniendo en cuenta estrategias múltiples para responder ante diversos públicos (comunicacional, jurídico, educativo, etc.)
+      </h3>
       <div class="lineadiv2"></div>
       <h3 class="actividadh3mobile"> <span class="fa-stack circulo1">
       <span class="fa fa-circle-o fa-stack-2x "></span>
       <strong class="fa-stack-1x">
-        2    
+        3    
       </strong>
       </span>Suban la propuesta al drive en formato PDF. No olviden escribir sus nombres. </h3>
       
       <h3 class="actividadh3"> <span class="fa-stack circulo1">
       <span class="fa fa-circle-o fa-stack-2x "></span>
       <strong class="fa-stack-1x">
-        2    
+        3    
       </strong>
       </span>Suban la propuesta al drive en formato PDF. No olviden escribir sus nombres. </h3>
 
